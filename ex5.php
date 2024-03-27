62abbf84abdf8b91c9811c626828dcb6a42f8a75
Felipe Neves <rj.felipeneves@gmail.com>
2024-03-27 00:55:54 -0300

    Remoção do html

--- ex5.php
+++ ex5.php
@@ -1,38 +1,24 @@
-<!DOCTYPE html>
-<html lang="pt-br">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>EXERCÍCIO 5 - DESAFIO DEV - FIBONACCI</title>
-</head>
+<?php
 
-<body>
-  <h1>
-    <?php
+function generateFibonacci($size)
+{
+  $fibonacci = array();
 
-    function generateFibonacci($size)
-    {
-      $fibonacci = array();
+  $fibonacci[0] = 1;
+  $fibonacci[1] = 1;
 
-      $fibonacci[0] = 1;
-      $fibonacci[1] = 1;
+  for ($i = 2; $i < $size; $i++) {
+    $fibonacci[$i] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
+  }
 
-      for ($i = 2; $i < $size; $i++) {
-        $fibonacci[$i] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
-      }
+  return $fibonacci;
+}
 
-      return $fibonacci;
-    }
+// Coloque aqui um valor númerico (Input)
+$input = 6;
 
-    // Coloque aqui um valor númerico (Input)
-    $input = 6;
+$result = generateFibonacci($input);
+echo "Output: [" . implode(", ", $result) . "]";
 
-    $result = generateFibonacci($input);
-    echo "Output: [" . implode(", ", $result) . "]";
-
-    ?>
-  </h1>
-</body>
-
-</html>
+?>
